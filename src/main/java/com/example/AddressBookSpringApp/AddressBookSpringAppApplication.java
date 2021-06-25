c12faee1026a2b125d5212960ada4d258878f252
Abhishek <ABHISHEKKUMAR998>
2021-06-26 01:17:48 +0530

    Add loombook library for logging

--- src/main/java/com/example/AddressBookSpringApp/AddressBookSpringAppApplication.java
+++ src/main/java/com/example/AddressBookSpringApp/AddressBookSpringAppApplication.java
@@ -2,12 +2,17 @@ package com.example.AddressBookSpringApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookSpringAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookSpringAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookSpringAppApplication.class, args);
+		log.info("Address Book App started in {} environment", context.getEnvironment().getProperty("environment"));
 	}
 
 }
