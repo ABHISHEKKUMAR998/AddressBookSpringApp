8a7ca998d8708a70fec3a2baa40d713b39535be9
Abhishek <ABHISHEKKUMAR998>
2021-06-26 02:02:31 +0530

    Setting Environt variables

--- src/main/java/com/example/AddressBookSpringApp/AddressBookSpringAppApplication.java
+++ src/main/java/com/example/AddressBookSpringApp/AddressBookSpringAppApplication.java
@@ -13,6 +13,7 @@ public class AddressBookSpringAppApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(AddressBookSpringAppApplication.class, args);
 		log.info("Address Book App started in {} environment", context.getEnvironment().getProperty("environment"));
+		log.info("Address Book DB User is {} ",context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
